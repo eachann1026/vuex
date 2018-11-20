73cfad5841c29072875db0226912ffebabd003c3
eachann <bye1997@qq.com>
2018-11-20 17:14:06 +0800

    优化写法

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -2,7 +2,10 @@
   <div>
     <h2>{{msg}}</h2>
     <hr />
-    <h3>{{$store.state.count}}</h3>
+
+    <!-- 优化前 ~~> <h3>{{$store.state.count}}</h3> -->
+    <!-- 优化后 ↓ -->
+    <h3>{{count}}</h3>
     <p>
       <!-- commit 是 方法 固定的写法 -->
       <button @click="$store.commit('add')">+</button>
@@ -20,11 +23,18 @@
         msg: "hello vuex"
       };
     },
+    computed:{
+      // 优化写法
+      count(){
+        return this.$store.state.count;
+      }
+    },
     mounted() {
       console.log(this.$store)
     },
     // 不是组件(components)可以直接注册
     store,
+
 
   };
 
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -5,7 +5,6 @@ Vue.use(Vuex);
 
 //  store --> 数据仓库,把公用的数据存起来,减少对服务器的调用
 //  vuex 适合中大型项目中使用  小型项目使用 vue stroe 会更轻便
-
 const state = { //公用的数据
   count: 1
 }
@@ -20,7 +19,7 @@ const mutations = {
   }
 }
 
-// export 暴露/公开
+// export default暴露/公开
 export default new Vuex.Store({
   state,
   mutations
